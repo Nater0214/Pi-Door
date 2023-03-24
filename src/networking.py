1d9e87b951086a5fc22a717c4625645e5add7f61
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-03-24 11:12:49 -0400

    Setup Post req for networking file Need to change server URL and port

---
+++ src/networking.py
@@ -0,0 +1,10 @@
+import requests as r
+
+# server listening on port 13337
+# posts door status to pi server machine
+def send_door_status(doorId, isOpen, timeStamp):
+    req = r.post('pi-server-url', json={
+        "door_id": doorId,
+        "open": isOpen,
+        "timestamp": timeStamp,
+    })
